ab9ee9d3220982cdde771323b2196264a506a73e
Emiliano Chontal <nanochontal@gmail.com>
2026-05-20 18:33:38 -0600

    Archivos

--- rama_dto.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type RamaDTO struct {
-	NombreRama  string `json:"nombre_rama"`
-	Descripcion string `json:"descripcion"`
-	AuditoriaID int    `json:"auditoria_id"`
-}
--- rol_dto.go
+++
@@ -1,7 +0,0 @@
-package dto
-
-type RolDTO struct {
-	NombreRol   string `json:"nombre_rol"`
-	Descripcion string `json:"descripcion"`
-	AuditoriaID int    `json:"auditoria_id"`
-}
